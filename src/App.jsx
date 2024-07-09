2d8d8d8f4b1302a7724cc8c791e57c87afcdf154
Mehedi Hasan <mhedih32@gmail.com>
2024-07-09 21:35:41 +0600

    lorem added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <DaisyNav></DaisyNav>
       <h1 className='text-red-600 font-bold text-4xl bg-green-400 text-center py-4'>Vite + React</h1>
+      {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, eos? Expedita, provident hic ratione fugiat veniam quam commodi ad ab eum officia harum earum saepe odio impedit voluptatibus autem mollitia in vel temporibus deserunt. Magni debitis perspiciatis dolorem dignissimos voluptatem. In tempore expedita modi? Quaerat illum totam expedita. Tempora, accusamus.</p> */}
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     ];
     return (
         <nav>
-
+            
         </nav>
     );
 };
