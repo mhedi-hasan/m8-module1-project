afda5ec9ef83317365d91914c5b541761e5ffa8f
Mehedi Hasan <mhedih32@gmail.com>
2024-07-09 20:06:09 +0600

    DaisyNav folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
 
   return (
     <>
-
+      <DaisyNav></DaisyNav>
       <h1 className='text-red-600 font-bold text-4xl bg-green-400 text-center py-4'>Vite + React</h1>
     </>
   )
