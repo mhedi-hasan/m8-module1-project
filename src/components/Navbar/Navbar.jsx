d202b46915ecc36a9de308c57be8cf8d4940f4a3
Mehedi Hasan <mhedih32@gmail.com>
2024-07-09 20:22:49 +0600

    new folder added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    const routes = [
+        { id: 1, path: "/", name: "Home" },
+        { id: 2, path: "/about", name: "About" },
+        { id: 3, path: "/services", name: "Services" },
+        { id: 4, path: "/contact", name: "Contact" },
+        { id: 5, path: "*", name: "NotFound" },
+    ];
+    return (
+        <nav>
+
+        </nav>
+    );
+};
+
+export default Navbar;
